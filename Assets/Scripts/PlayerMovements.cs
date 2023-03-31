88a6b2690fd675b8345882772a83d9e1737c0968
Jennifyy <JennaStillings@yahoo.com>
2023-03-31 10:42:35 -0700

    quickupdateRAHHHH

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity= new Vector3(0,10,0);
+        }
+    }
+}
