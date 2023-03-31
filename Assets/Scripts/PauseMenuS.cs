23314c941bf4eeb011d383f072ee94cb4f868da1
Jennifyy <JennaStillings@yahoo.com>
2023-03-31 00:08:29 -0700

    PreAlphaUpdate
    Added: Menu, Pause/Resume Screen, Exit, Settings with volume, Button for tutorial so it will show tutorial scene.

---
+++ Assets/Scripts/PauseMenuS.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuS : MonoBehaviour
+{
+
+    public static bool Paused = false;
+    public GameObject PauseMenuCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused)
+            {
+                Play();
+            }
+            else
+            {
+                stop();
+            }
+        }
+    }
+
+    void stop()
+    {
+        PauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        Paused = true;
+        Cursor.visible = true;
+    }
+    
+    public void Play()
+    {
+        PauseMenuCanvas.SetActive(false);
+            Time.timeScale = 01f;
+        Paused = false;
+        Cursor.visible = false;
+    }
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
+}
---
+++ Assets/Scripts/QuitSceneLoader.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitSceneLoader : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+}
+
