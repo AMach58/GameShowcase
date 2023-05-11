8003cac405380f85200ccc56ce157d8fdc671cff
Jennifyy <JennaStillings@yahoo.com>
2023-05-11 01:02:31 -0700

    LeveloneCompleteForNow

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -5,23 +5,48 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
     private bool grounded;
+    private Animator anim;
+
+    private float dirX = 0f;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxis("Horizontal");
+        dirX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump") && grounded)
         {
             Jump();
+        }
+
+        UpdateAnimationUpdate();
+    }
+    private void UpdateAnimationUpdate()
+    {
+        if(dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 
@@ -32,7 +57,7 @@ public class PlayerMovements : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
+    { 
         if (collision.gameObject.tag == "Platform")
         {
             grounded = true;
