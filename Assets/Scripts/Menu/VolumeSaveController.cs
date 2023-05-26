649d1622d66180b95e3454d22e84ef6254ea081f
Jennifyy <JennaStillings@yahoo.com>
2023-05-26 12:15:11 -0700

    Sound DONT WORKK AHDAHDHAHDAHDUA

--- Assets/Scripts/Menu/VolumeSaveController.cs
+++ Assets/Scripts/Menu/VolumeSaveController.cs
@@ -8,10 +8,18 @@ public class VolumeSaveController : MonoBehaviour
     [SerializeField] private Slider volumeSlider = null;
 
     [SerializeField] private Text volumeTextUI = null;
+
+    public GameObject myCamera;
+    private AudioSource MyAudio;
     
-    public void start()
+    public void Start()
     {
         LoadValues();
+        //myCamera = GameObject.Find("Main Camera");
+        
+        MyAudio = GetComponent<AudioSource>();
+       // MyAudio = myCamera.GetComponent<AudioSource>();
+
     }
 
     public void VolumeSlider(float volume)
@@ -30,6 +38,7 @@ public class VolumeSaveController : MonoBehaviour
     {
         float volumeValue = PlayerPrefs.GetFloat("volumeValue");
         volumeSlider.value = volumeValue;
-        AudioListener.volume = volumeValue;
+        //AudioListener.volume = volumeValue;
+        MyAudio.volume = volumeValue;
     }
 }
